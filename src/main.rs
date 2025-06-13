e3560ea4391488124deff23bc2d87cf07c076cf8
Michel Lao <SansNom01@proton.me>
2025-06-13 19:05:32 +0200

    app: fix read command line arguments

--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,21 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(gh_webhook)
     });
-    //.bind((server_host, server_port.parse().unwrap()))?
+
     if let Some(config) = get_config() {
         let mut builder = SslAcceptor::mozilla_modern_v5(SslMethod::tls()).unwrap();
         builder
             .set_private_key_file(config.private_key_file, SslFiletype::PEM)
             .expect("Private key file unset");
-        builder.set_certificate_chain_file(config.chain_key_file).expect("Certificate chain file unset");
+        builder
+            .set_certificate_chain_file(config.chain_key_file)
+            .expect("Certificate chain file unset");
+        println!("Started TLS Server on {}:{}", server_host, server_port);
         http_server.bind_openssl((server_host, server_port.parse().unwrap()), builder)?
             .run()
             .await
     } else {
+        println!("Started HTTP Server on {}:{}", server_host, server_port);
         http_server.bind((server_host, server_port.parse().unwrap()))?
             .run()
             .await
@@ -40,10 +44,10 @@ async fn main() -> std::io::Result<()> {
 
 fn get_config() -> Option<Config> {
     let mut args = std::env::args();
-    if let Some(position_key) = args.position(|arg| arg == "--private-key") {
-        let private_value_file = args.nth(position_key + 1).expect("Private key file unset");
-        if let Some(position_key) = args.position(|arg| arg == "--fullchain-key") {
-            let chain_value_file = args.nth(position_key + 1).expect("Fullchain key file unset");
+    if let Some(_) = args.position(|arg| arg == "--private-key") {
+        let private_value_file = args.next().expect("");
+        if let Some(_) = args.position(|arg| arg == "--fullchain-key") {
+            let chain_value_file = args.next().expect("Fullchain key file unset");
             return Some(Config {
                 private_key_file: private_value_file,
                 chain_key_file: chain_value_file,
